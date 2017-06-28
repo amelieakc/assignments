8e8f38456427cff21aa5c0edb2e91958c74d1399
Amelie Cameron <amelieakc@gmail.com>
2017-06-28 10:25:56 -0700

    ascii

--- CSC415/ASCIICount.c
+++ CSC415/ASCIICount.c
@@ -6,40 +6,40 @@
 
 void *runner(void *param); /* threads call this function */
 const int size = 65536;
-char buffer[size] = {};
-
+char buffer[size] = {0};
+int globalCounter[size] = {0};
 
 typedef struct ThreadArgs {
+  int tid;
   int left;
   int right;
-  
 } ThreadArgs;
 
-
-int* countBuffer(const int left, const int right){
+/*
+  Combine all of the counter spaces
+ */
+int* reduce() {
   int* countArray = malloc(128 * sizeof(int));
-
-  for (int i = left; i < right; ++i)
-    {
-      int temp = 0;
-      temp = buffer[i];
-      //printf("%d ", buffer[i]);
-      if((temp >= 33) && (temp <= 126))
-	countArray[temp]++;
+  
+  // clear count array
+  for(int i = 0; i < 128; ++i)
+    countArray[i] = 0;
+  
+  for(int i = 33; i <= 126; ++i){
+    for(int tid = 1; tid <= 8; ++tid){  
+      countArray[i] += globalCounter[i*tid];
     }
-
+  }
   return countArray;
 }
 
 
 int main(int argc, char *argv[])
 {
-  pthread_t tid; /* the thread identifier */
   pthread_attr_t attr; /* set of thread attributes */
-  
+  pthread_t* threads = malloc(THREADS * sizeof(pthread_t));
+
   FILE *test;
-  //char buffer[size] = {};
-  //int countArray[size] = {};
   
   test = fopen("test.txt", "r");
   if(test == NULL){
@@ -53,15 +53,6 @@ int main(int argc, char *argv[])
     fclose(test);
   }
 
-  int* countArray = countBuffer(0, size);
-  for (int i = 0; i < size; ++i){
-    if(countArray[i] != 0){
-      char c = i;
-      printf("%c : ", c);
-      printf("%d \n", countArray[i]);
-    }
-  }
-
   /* get the default attributes */
   pthread_attr_init(&attr);
 
@@ -71,15 +62,34 @@ int main(int argc, char *argv[])
   /* create the threads */
   for(int i = 0; i < THREADS; ++i){
     struct ThreadArgs* args = (struct ThreadArgs*)malloc(sizeof(struct ThreadArgs));
+    args->tid = i+1;
     args->left = i*chunk_size;
     args->right = i*chunk_size + chunk_size;
 
-    pthread_create(&tid, &attr, runner, args);
-    printf("created thread ");
-    printf("%d\n",i+1);
+    pthread_create(&threads[i], &attr, runner, args);
+    //    printf("created thread ");
+    //printf("%d\n",i+1);
   }
   /* wait for the thread to exit */
-  pthread_join(tid,NULL);
+  for(int i = 0; i < THREADS; ++i)
+    pthread_join(threads[i], NULL);
+
+  // check the global counter
+  // DEBUGGING
+  for(int i = 0; i < size; ++i) {
+    if (globalCounter[i] != 0)
+      printf("%d:%d\n", i, globalCounter[i]);
+  }
+
+  int *countArray = reduce();
+  for (int i = 0; i < size; ++i){
+    if(countArray[i] != 0){
+      char c = i;
+      printf("%d : ", i);
+      printf("%c : ", c);
+      printf("%d \n", countArray[i]);
+    }
+  }
 }
 
 
@@ -88,16 +98,21 @@ int main(int argc, char *argv[])
 void *runner(void *args)
 {
   ThreadArgs* mapper_args = args;
-  printf("%d %d\n", mapper_args->left, mapper_args->right);
-  int* countArray = malloc(128 * sizeof(int));
+  //printf("I'm thread %d\n", mapper_args->tid);
+
+  //printf("%d %d\n", mapper_args->left, mapper_args->right);
 
   for (int i = mapper_args->left; i < mapper_args->right; ++i)
     {
       int temp = 0;
       temp = buffer[i];
-      //printf("%d ", buffer[i]);
-      if((temp >= 33) && (temp <= 126))
-	countArray[temp]++;
+      printf("%c ", buffer[i]);
+      if((temp >= 33) && (temp <= 126)) {
+	//printf("%c\n", (char)temp);
+	//printf("%d\n", temp);
+	int counter_index = temp * mapper_args->tid;
+	globalCounter[counter_index]++;
+      }
     }
   
   pthread_exit(0);
